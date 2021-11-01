22bc8dbd70fd8c0e128c3fe3b471cfb3605f7c21
Louis CHOMEL <louis.chomel@gmail.com>
2021-11-01 15:45:27 +0100

    Creating mongoose word model

--- api/models/wordModel.js
+++ api/models/wordModel.js
@@ -1,1 +1,23 @@
 //Build Mongoose Schema here
+const mongoose = require('mongoose');
+
+const WordSchema = mongoose.Schema({
+    kor : {
+        type : String,
+        required : true
+    },
+    eng : {
+        type : String
+    },
+    wordType : {
+        type : String,
+        required : true
+    },
+    date : {
+        type: Date,
+        default : Date.now
+
+    },
+});
+
+module.exports = mongoose.model('wordModel',WordSchema);
